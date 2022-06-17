5f29ddf74c85b75443d41fa828772e8f748895fd
md_mumtaj_alam <mmumtajalam987@gmail.com>
2022-06-17 19:17:59 +0530

    done

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
-const fname = document.getElementById('fname');
-const lname = document.getElementById('lname');
-const Btnsubmit = document.getElementById('btnsubmit');
+const fname = document.getElementById("fname");
+const lname = document.getElementById("lname");
+const Btnsubmit = document.getElementById("btnsubmit");
+const ageIn = document.getElementById("dob");
 
-
-Btnsubmit.addEventListener('click', ()=>{
-    let name = (fname.value)
-})
+Btnsubmit.addEventListener("click", () => {
+  let name = fname.value + " " + lname.value;
+  let dob = ageIn.value;
+  var today = new Date();
+  let thisyr = today.getFullYear();
+  let birthyr = dob.substr(0, 4);
+  let age = thisyr - parseInt(birthyr);
+  alert("My name is " + name + "and my age is " + age);
+});
